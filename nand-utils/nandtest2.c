014772694e9d2b5020d7381f36741f75f6a902e7
Woody Wu <woody.wu2@honeywell.com>
2020-08-06 04:09:45 +0800

    nandtest2: mark bad blocks

--- nand-utils/nandtest2.c
+++ nand-utils/nandtest2.c
@@ -12,7 +12,6 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <getopt.h>
-#include <stdbool.h>
 
 #include <asm/types.h>
 #include "mtd/mtd-user.h"
@@ -28,11 +27,10 @@ static NORETURN void usage(int status)
         "  -h, --help           Display this help output\n"
         "  -V, --version        Display version information and exit\n"
         "  -m, --markbad        Mark blocks bad if they appear so\n"
-        "  -p, --passes         Number of passes\n"
-        "  -r <n>, --reads=<n>  Read & check <n> times per pass\n"
-        "  -o, --offset         Start offset on flash\n"
-        "  -l, --length         Length of flash to test\n"
-        "  -k, --keep           Restore existing contents after test\n"
+        "  -p, --passes         Number of passes [default=1]\n"
+        "  -r <n>, --reads=<n>  Read operation repeats time [default=3]\n"
+        "  -s, --start          Start index of block [default=0]\n"
+        "  -n, --blocks         number of blocks to test [default=flash blocks number]\n"
         "  -S, --info-only      Only print MTD information, no test\n",
         PROGRAM_NAME);
     exit(status);
@@ -49,7 +47,7 @@ static int markbad;
 static libmtd_t mtd_desc;
 static struct mtd_dev_info mtd;
 static unsigned char *oobbuf;
-static bool *bbt;
+static unsigned char *bbt;
 static bool info_only;
 
 /**
@@ -152,115 +150,7 @@ static int aligned_read_and_compare(int page_start, int page_end, void *data)
     free(buf);
     free(rbuf);
 
-    if (fail_cnt == nr_reads) {
-        ; /* markbad */
-    }
-
-    return 0;
-}
-
-static int read_and_compare(loff_t ofs, unsigned char *data,
-                unsigned char *rbuf)
-{
-    ssize_t len;
-    int i;
-
-    len = pread(fd, rbuf, meminfo.erasesize, ofs);
-    if (len < meminfo.erasesize) {
-        printf("\n");
-        if (len)
-            fprintf(stderr, "Short read (%zd bytes)\n", len);
-        else
-            perror("read");
-        exit(1);
-    }
-
-    if (ioctl(fd, ECCGETSTATS, &newstats)) {
-        printf("\n");
-        perror("ECCGETSTATS");
-        close(fd);
-        exit(1);
-    }
-
-    if (newstats.corrected > oldstats.corrected) {
-        printf("\n %d bit(s) ECC corrected at %08x\n",
-                newstats.corrected - oldstats.corrected,
-                (unsigned) ofs);
-        oldstats.corrected = newstats.corrected;
-    }
-    if (newstats.failed > oldstats.failed) {
-        printf("\nECC failed at %08x\n", (unsigned) ofs);
-        oldstats.failed = newstats.failed;
-    }
-
-    printf("\r%08x: checking...", (unsigned)ofs);
-    fflush(stdout);
-
-    if (memcmp(data, rbuf, meminfo.erasesize)) {
-        printf("\n");
-        fprintf(stderr, "compare failed\n");
-        for (i=0; i<meminfo.erasesize; i++) {
-            if (data[i] != rbuf[i])
-                printf("Byte 0x%x is %02x should be %02x\n",
-                       i, rbuf[i], data[i]);
-        }
-        return 1;
-    }
-    return 0;
-}
-
-static int erase_and_write(loff_t ofs, unsigned char *data, unsigned char *rbuf,
-               int nr_reads)
-{
-    struct erase_info_user er;
-    ssize_t len;
-    int i, read_errs = 0;
-
-    printf("\r%08x: erasing... ", (unsigned)ofs);
-    fflush(stdout);
-
-    er.start = ofs;
-    er.length = meminfo.erasesize;
-
-    if (ioctl(fd, MEMERASE, &er)) {
-        perror("MEMERASE");
-        if (markbad) {
-            printf("Mark block bad at %08lx\n", (long)ofs);
-            ioctl(fd, MEMSETBADBLOCK, &ofs);
-        }
-        return 1;
-    }
-
-    printf("\r%08x: writing...", (unsigned)ofs);
-    fflush(stdout);
-
-    len = pwrite(fd, data, meminfo.erasesize, ofs);
-    if (len < 0) {
-        printf("\n");
-        perror("write");
-        if (markbad) {
-            printf("Mark block bad at %08lx\n", (long)ofs);
-            ioctl(fd, MEMSETBADBLOCK, &ofs);
-        }
-        return 1;
-    }
-    if (len < meminfo.erasesize) {
-        printf("\n");
-        fprintf(stderr, "Short write (%zd bytes)\n", len);
-        exit(1);
-    }
-
-    for (i=1; i<=nr_reads; i++) {
-        printf("\r%08x: reading (%d of %d)...", (unsigned)ofs, i, nr_reads);
-        fflush(stdout);
-        if (read_and_compare(ofs, data, rbuf))
-            read_errs++;
-    }
-    if (read_errs) {
-        fprintf(stderr, "read/check %d of %d failed.\n", read_errs, nr_reads);
-        return 1;
-    }
-    return 0;
+    return fail_cnt < nr_reads ? 0 : -1;
 }
 
 /**
@@ -271,6 +161,7 @@ static int scan_badblocks(void)
     int bb_cnt = 0;
     int err;
 
+    printf("scanning bad blocks: ");
     for (int i = 0; i < mtd.eb_cnt; ++i) {
         /* bad block mark could be in 1st or 2st page */
         for (int j = 0; j < 2; ++j) {
@@ -282,7 +173,7 @@ static int scan_badblocks(void)
             }
             if (oobbuf[0] != 0xff) {
                 if (! bb_cnt)
-                    printf("bad blocks: %d", i);
+                    printf("%d", i);
                 else
                     printf(", %d", i);
                 if ((err = mtd_is_bad(&mtd, fd, i)) < 0) {
@@ -293,7 +184,7 @@ static int scan_badblocks(void)
                     fprintf(stderr, "\nbad block status mismatched between mtd and oob\n");
                     return -1;
                 }
-                bbt[i] = true;
+                bbt[i] = 1;
                 ++bb_cnt;
                 break;
             }
@@ -313,7 +204,6 @@ static int scan_badblocks(void)
 static int erase_block(int block)
 {
     struct erase_info_user er;
-    loff_t ofs;
 
     er.start = block * meminfo.erasesize;
     er.length = meminfo.erasesize;
@@ -321,12 +211,6 @@ static int erase_block(int block)
     printf("erasing block %d\n", block);
     if (ioctl(fd, MEMERASE, &er)) {
         perror("MEMERASE");
-        if (markbad) {
-            printf("mark bad block %d\n", block);
-            ofs = er.start;
-            if (ioctl(fd, MEMSETBADBLOCK, &ofs))
-                perror("MEMSETBADBLOCK");
-        }
         return -1;
     }
     return 0;
@@ -392,19 +276,15 @@ static int aligned_write(int page_start, int page_end, void *data)
     if (n == -1) {
         sprintf(str, "write error on pages [%d, %d]\n", page_start, page_end);
         perror(str);
-        goto fail;
+        exit(1);
     } else if (n < len) {
         fprintf(stderr, "write error on pages [%d, %d]. len %ld actual %ld\n"
                 , page_start, page_end, sizeof(buf), n);
-        goto fail;
+        exit(1);
     }
 
     free(buf);
     return 0;
-
-fail:
-    free(buf);
-    return -1;
 }
 
 static int scratch_block(int block)
@@ -412,10 +292,13 @@ static int scratch_block(int block)
     int page_start = block * (meminfo.erasesize / meminfo.writesize);
     int page_end = page_start + (meminfo.erasesize / meminfo.writesize) - 1;
 
-    random_tree_walk_with_intvl(page_start, page_end, aligned_write
-            , (void *)(long int)seeds[block]);
-    random_tree_walk_with_intvl(page_start, page_end, aligned_read_and_compare
-            , (void *)(long int)seeds[block]);
+    if (random_tree_walk_with_intvl(page_start, page_end, aligned_write,
+                (void *)(long int)seeds[block]))
+        return -1;
+    if (random_tree_walk_with_intvl(page_start, page_end, aligned_read_and_compare,
+                (void *)(long int)seeds[block]))
+        return -1;
+
     return 0;
 }
 
@@ -423,11 +306,40 @@ static int test_block(int block, void *nouse)
 {
     UNUSED(nouse);
 
-    if (bbt[block])
+    if (bbt[block]) {
         printf("skip bad block %d\n", block);
-    else if (! erase_block(block))
-        scratch_block(block);
+        return 0;
+    }
+    if (! erase_block(block) && ! scratch_block(block))
+        return 0;
+
+    if (markbad) {
+        printf("Mark bad block %d\n", block);
+        if (ioctl(fd, MEMSETBADBLOCK, block * meminfo.erasesize)) {
+            perror("MEMSETBADBLOCK");
+            exit(1);
+        }
+        bbt[block] = 1;
+    }
     return 0;
+}
+
+static void get_ecc_info(void)
+{
+    if (ioctl(fd, ECCGETSTATS, &oldstats)) {
+        perror("ECCGETSTATS");
+        close(fd);
+        exit(1);
+    }
+
+    printf("ecc: corrections %d failures %d bad blocks %d bbt blocks %d\n",
+            oldstats.corrected, oldstats.failed, oldstats.badblocks,
+            oldstats.bbtblocks);
+
+    if (scan_badblocks()) {
+        fprintf(stderr, "bad blocks info mismatched between kernel and oob\n");
+        exit(1);
+    }
 }
 
 /*
@@ -435,26 +347,21 @@ static int test_block(int block, void *nouse)
  */
 int main(int argc, char **argv)
 {
-    int i;
-    unsigned char *wbuf, *rbuf, *kbuf;
-    int pass;
     int nr_passes = 1;
-    int keep_contents = 0;
-    uint32_t offset = 0;
-    uint32_t length = -1;
+    uint32_t block_start = 0;
+    uint32_t block_nr = -1;
     int error = 0;
 
     for (;;) {
-        static const char short_options[] = "hkl:mo:p:r:VS";
+        static const char short_options[] = "hn:ms:p:r:VS";
         static const struct option long_options[] = {
             { "help", no_argument, 0, 'h' },
             { "version", no_argument, 0, 'V' },
             { "markbad", no_argument, 0, 'm' },
             { "passes", required_argument, 0, 'p' },
-            { "offset", required_argument, 0, 'o' },
-            { "length", required_argument, 0, 'l' },
+            { "start", required_argument, 0, 's' },
+            { "blocks", required_argument, 0, 'n' },
             { "reads", required_argument, 0, 'r' },
-            { "keep", no_argument, 0, 'k' },
             { "info-only", no_argument, 0, 'S' },
             {0, 0, 0, 0},
         };
@@ -477,10 +384,6 @@ int main(int argc, char **argv)
             markbad = 1;
             break;
 
-        case 'k':
-            keep_contents = 1;
-            break;
-
         case 'p':
             nr_passes = atol(optarg);
             break;
@@ -489,12 +392,12 @@ int main(int argc, char **argv)
             nr_reads = atol(optarg);
             break;
 
-        case 'o':
-            offset = simple_strtoul(optarg, &error);
+        case 's':
+            block_start = simple_strtoul(optarg, &error);
             break;
 
-        case 'l':
-            length = simple_strtoul(optarg, &error);
+        case 'n':
+            block_nr = simple_strtoul(optarg, &error);
             break;
 
         case 'S':
@@ -520,28 +423,10 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    if (length == -1)
-        length = meminfo.size;
+    if (block_nr == -1) block_nr = meminfo.size / meminfo.erasesize;
 
-    if (offset % meminfo.erasesize) {
-        fprintf(stderr, "Offset %x not multiple of erase size %x\n",
-            offset, meminfo.erasesize);
-        exit(1);
-    }
-    if (length % meminfo.erasesize) {
-        fprintf(stderr, "Length %x not multiple of erase size %x\n",
-            length, meminfo.erasesize);
-        exit(1);
-    }
-    if (length + offset > meminfo.size) {
-        fprintf(stderr, "Length %x + offset %x exceeds device size %x\n",
-            length, offset, meminfo.size);
-        exit(1);
-    }
-
-    if (ioctl(fd, ECCGETSTATS, &oldstats)) {
-        perror("ECCGETSTATS");
-        close(fd);
+    if (block_start + block_nr > meminfo.size / meminfo.erasesize) {
+        fprintf(stderr, "block positon out of range\n");
         exit(1);
     }
 
@@ -563,23 +448,11 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    if (! (bbt = (bool *)malloc(mtd.eb_cnt))) {
+    if (! (bbt = (unsigned char *)malloc(mtd.eb_cnt))) {
         fprintf(stderr, "out of memory\n");
         exit(1);
     }
     memset(bbt, 0, mtd.eb_cnt);
-
-    printf("memory: type %u flags 0x%x size %u erasesize %u writesize %u oobsize %u\n",
-            meminfo.type, meminfo.flags, meminfo.size, meminfo.erasesize,
-            meminfo.writesize, meminfo.oobsize); 
-    printf("mtd: size %lld eb_cnt %d eb_size %d min_io_size %d"
-            " subpage_size %d oob_size %d bb_allowed %d\n",
-            mtd.size, mtd.eb_cnt, mtd.eb_size, mtd.min_io_size,
-            mtd.subpage_size, mtd.oob_size, mtd.bb_allowed);
-    printf("oob: useecc %d eccbytes %u\n", oobinfo.useecc, oobinfo.eccbytes);
-    printf("ecc: corrections %d failures %d bad blocks %d bbt blocks %d\n",
-            oldstats.corrected, oldstats.failed, oldstats.badblocks,
-            oldstats.bbtblocks);
 
     if (meminfo.size != mtd.size || meminfo.erasesize != mtd.eb_size
             || meminfo.writesize != mtd.min_io_size) {
@@ -593,72 +466,33 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    printf("scanning bad blocks from oob\n");
-    if (scan_badblocks()) {
-        fprintf(stderr, "bad blocks info mismatched between kernel and oob\n");
-        exit(1);
-    }
+    printf("memory: type %u flags 0x%x size %u erasesize %u writesize %u oobsize %u\n",
+            meminfo.type, meminfo.flags, meminfo.size, meminfo.erasesize,
+            meminfo.writesize, meminfo.oobsize); 
+    printf("mtd: size %lld eb_cnt %d eb_size %d min_io_size %d"
+            " subpage_size %d oob_size %d bb_allowed %d\n",
+            mtd.size, mtd.eb_cnt, mtd.eb_size, mtd.min_io_size,
+            mtd.subpage_size, mtd.oob_size, mtd.bb_allowed);
+    printf("oob: useecc %d eccbytes %u\n", oobinfo.useecc, oobinfo.eccbytes);
+
+    get_ecc_info();
 
     if (info_only) return 0;
-
-    wbuf = malloc(meminfo.erasesize * 3);
-    if (!wbuf) {
-        fprintf(stderr, "Could not allocate %d bytes for buffer\n",
-            meminfo.erasesize * 2);
-        exit(1);
-    }
-    rbuf = wbuf + meminfo.erasesize;
-    kbuf = rbuf + meminfo.erasesize;
 
     if (! (seeds = malloc(mtd.eb_cnt * sizeof(int)))) {
         fprintf(stderr, "out of memory in allocating seeds series\n");
         exit(1);
     }
 
-    int *p = seeds;
-    srand(time(NULL));
-    for (int i = 0; i < mtd.eb_cnt; i++) *p++ = rand();
+    for (int i = 0; i < nr_passes; ++i) {
+        int *p = seeds;
+        srand(time(NULL));
+        for (int j = 0; j < mtd.eb_cnt; j++) *p++ = rand();
 
-    srandom(time(NULL));
-    return random_tree_walk(0, mtd.eb_cnt - 1, test_block, NULL);
-
-    for (pass = 0; pass < nr_passes; pass++) {
-        loff_t test_ofs;
-
-        for (test_ofs = offset; test_ofs < offset+length; test_ofs += meminfo.erasesize) {
-            ssize_t len;
-
-            srand(time(NULL));
-
-            if (ioctl(fd, MEMGETBADBLOCK, &test_ofs)) {
-                printf("\rBad block at 0x%08x\n", (unsigned)test_ofs);
-                continue;
-            }
-
-            for (i=0; i<meminfo.erasesize; i++)
-                wbuf[i] = rand();
-
-            if (keep_contents) {
-                printf("\r%08x: reading... ", (unsigned)test_ofs);
-                fflush(stdout);
-
-                len = pread(fd, kbuf, meminfo.erasesize, test_ofs);
-                if (len < meminfo.erasesize) {
-                    printf("\n");
-                    if (len)
-                        fprintf(stderr, "Short read (%zd bytes)\n", len);
-                    else
-                        perror("read");
-                    exit(1);
-                }
-            }
-            if (erase_and_write(test_ofs, wbuf, rbuf, nr_reads))
-                continue;
-            if (keep_contents)
-                erase_and_write(test_ofs, kbuf, rbuf, 1);
-        }
-        printf("\nFinished pass %d successfully\n", pass+1);
+        srandom(time(NULL));
+        random_tree_walk(block_start, block_start + block_nr - 1, test_block, NULL);
+        printf("finished pass %d\n", i + 1);
     }
-    /* Return happy */
-    return 0;
+
+    get_ecc_info();
 }
